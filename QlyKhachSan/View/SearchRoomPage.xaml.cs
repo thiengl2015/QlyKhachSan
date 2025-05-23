6a17889c28ef22eb1a48073d82c04f67c5220897
Gnuttttt <dinhthanhtung0312@gmail.com>
2025-05-23 23:02:02 +0700

    Hoan thanh UI Menu va Tra cuu Phong

--- QlyKhachSan/View/SearchRoomPage.xaml.cs
+++ QlyKhachSan/View/SearchRoomPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using QlyKhachSan.View;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QlyKhachSan
+namespace QlyKhachSan.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SearchRoomPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SearchRoomPage : Page
     {
-        public MainWindow()
+        public SearchRoomPage()
         {
             InitializeComponent();
-            Mainpage.Content = new SearchRoomPage();
         }
     }
 }
