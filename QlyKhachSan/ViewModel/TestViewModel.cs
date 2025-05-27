ed8a3ab5ae5064dca517ec26985145c8bece3c3d
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-05-27 09:17:16 +0700

    Add testviewmodel

---
+++ QlyKhachSan/ViewModel/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QlyKhachSan.ViewModel
+{
+    public class TestViewModel : BaseViewModel
+    {
+        
+    }
+}
