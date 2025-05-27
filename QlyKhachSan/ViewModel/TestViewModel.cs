fb3ff425bc5f6a8811e16226a6afa6511fee0cae
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-05-27 10:19:23 +0700

    Test database

--- QlyKhachSan/ViewModel/TestViewModel.cs
+++ QlyKhachSan/ViewModel/TestViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QlyKhachSan.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,20 @@ namespace QlyKhachSan.ViewModel
 {
     public class TestViewModel : BaseViewModel
     {
-        
+        private ObservableCollection<LOAIPHONG> _list;
+        public ObservableCollection<LOAIPHONG> List
+        {
+            get { return _list; }
+            set
+            {
+                _list = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TestViewModel()
+        {
+            List = new ObservableCollection<LOAIPHONG>(DataProvider.Instance.DB.LOAIPHONGs.ToList());
+        }
     }
 }
