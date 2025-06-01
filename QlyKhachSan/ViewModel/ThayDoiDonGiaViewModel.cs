9f8273bbe31fe030f086142480ec97e4058f2449
Phan Võ Mỹ Huyền <22520591@gm.uit.edu.vn>
2025-06-01 22:53:10 +0700

    UI Thay đổi đơn giá phòng

---
+++ QlyKhachSan/ViewModel/ThayDoiDonGiaViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace QlyKhachSan.ViewModel
+{
+    public class ThayDoiDonGiaViewModel : INotifyPropertyChanged
+    {
+        private string _donGia;
+        public string DonGia
+        {
+            get => _donGia;
+            set
+            {
+                _donGia = value;
+                OnPropertyChanged(nameof(DonGia));
+            }
+        }
+
+        public ICommand ThayDoiCommand { get; }
+
+        public ThayDoiDonGiaViewModel()
+        {
+            ThayDoiCommand = new RelayCommand(ThayDoi);
+            DonGia = "<Giá trị cũ>"; // Giá trị mặc định
+        }
+
+        private void ThayDoi()
+        {
+            // Thực hiện xử lý thay đổi đơn giá
+            System.Windows.MessageBox.Show($"Đã thay đổi đơn giá thành: {DonGia}");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
