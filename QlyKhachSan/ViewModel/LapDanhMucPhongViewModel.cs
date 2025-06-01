01d27e7d3b0a0bb8fb1ad221e8c7a428f23f7bbf
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-06-01 16:04:33 +0700

    Thêm LapDMP viewmodel

---
+++ QlyKhachSan/ViewModel/LapDanhMucPhongViewModel.cs
@@ -0,0 +1,43 @@
+﻿using QlyKhachSan.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QlyKhachSan.ViewModel
+{
+    public class LapDanhMucPhongViewModel : BaseViewModel
+    {
+        private ObservableCollection<LOAIPHONG> danhSachLoaiPhong;
+        public ObservableCollection<LOAIPHONG> DanhSachLoaiPhong
+        {
+            get { return danhSachLoaiPhong; }
+            set
+            {
+                danhSachLoaiPhong = value;
+                OnPropertyChanged(nameof(DanhSachLoaiPhong));
+            }
+        }
+
+        private string _selectedLoaiPhong;
+        public string SelectedLoaiPhong
+        {
+            get => _selectedLoaiPhong;
+            set
+            {
+                _selectedLoaiPhong = value;
+                OnPropertyChanged(nameof(SelectedLoaiPhong));
+            }
+        }
+
+        public LapDanhMucPhongViewModel()
+        {
+            DanhSachLoaiPhong = new ObservableCollection<LOAIPHONG>(DataProvider.Instance.DB.LOAIPHONGs.ToList());
+
+            SelectedLoaiPhong = DanhSachLoaiPhong.First().TenLoaiPhong;
+        }
+    }
+}
