701adaf7ea5775d326ebc01a3607dc6fe46959ea
thiengl2015 <giathien899@gmail.com>
2025-06-01 21:41:15 +0700

    Xong tra cứu phòng

--- QlyKhachSan/View/TraCuuPhongPage.xaml.cs
+++ QlyKhachSan/View/TraCuuPhongPage.xaml.cs
@@ -25,37 +25,8 @@ namespace QlyKhachSan.View
         public TraCuuPhongPage()
         {
             InitializeComponent();
+            this.DataContext = new TraCuuPhongViewModel(); 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            TraCuuPhong();
-        }
-
-        private void TraCuuPhong()
-        {
-            using (var db = new QuanLyKhachSanEntities())
-            {
-                var query = from p in db.PHONGs
-                            join lp in db.LOAIPHONGs on p.MaLoaiPhong equals lp.MaLoaiPhong
-                            join pt in db.PHIEUTHUEs on p.MaPhong equals pt.MaPhong into ptGroup
-                            from pt in ptGroup.DefaultIfEmpty()
-                            where
-                                (string.IsNullOrWhiteSpace(txtMaPhong.Text) || p.MaPhong.Contains(txtMaPhong.Text)) &&
-                                (string.IsNullOrWhiteSpace(txtTenPhong.Text) || p.TenPhong.Contains(txtTenPhong.Text)) &&
-                                (string.IsNullOrWhiteSpace(txtMaLoaiPhong.Text) || p.MaLoaiPhong.Contains(txtMaLoaiPhong.Text)) &&
-                                (cbTenLoaiPhong.SelectedItem == null || lp.TenLoaiPhong.Contains(cbTenLoaiPhong.Text)) &&
-                                (string.IsNullOrWhiteSpace(txtGhiChu.Text) || p.GhiChu.Contains(txtGhiChu.Text))
-                            select new PhongDTO
-                            {
-                                TenPhong = p.TenPhong,
-                                TenLoaiPhong = lp.TenLoaiPhong,
-                                DonGia = lp.DonGia ?? 0,
-                                TinhTrang = pt != null ? "Đang thuê" : "Trống"
-                            };
-
-                phong.ItemsSource = query.ToList();
-            }
-        }
     }
 }
---
+++ QlyKhachSan/ViewModel/TraCuuPhongViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using QlyKhachSan.Model;
+
+namespace QlyKhachSan.ViewModel
+{
+    public class TraCuuPhongViewModel : BaseViewModel
+    {
+        // Các thuộc tính tìm kiếm
+        private string _maPhong;
+        public string MaPhong
+        {
+            get => _maPhong;
+            set
+            {
+                _maPhong = value;
+                OnPropertyChanged(nameof(MaPhong));
+            }
+        }
+
+        private string _tenPhong;
+        public string TenPhong
+        {
+            get => _tenPhong;
+            set
+            {
+                _tenPhong = value;
+                OnPropertyChanged(nameof(TenPhong));
+            }
+        }
+
+        private string _maLoaiPhong;
+        public string MaLoaiPhong
+        {
+            get => _maLoaiPhong;
+            set
+            {
+                _maLoaiPhong = value;
+                OnPropertyChanged(nameof(MaLoaiPhong));
+            }
+        }
+
+        private string _tenLoaiPhong;
+        public string TenLoaiPhong
+        {
+            get => _tenLoaiPhong;
+            set
+            {
+                _tenLoaiPhong = value;
+                OnPropertyChanged(nameof(TenLoaiPhong));
+            }
+        }
+
+        private string _ghiChu;
+        public string GhiChu
+        {
+            get => _ghiChu;
+            set
+            {
+                _ghiChu = value;
+                OnPropertyChanged(nameof(GhiChu));
+            }
+        }
+
+        // Danh sách phòng tìm thấy
+        private ObservableCollection<PHONG> _danhSachPhong;
+        public ObservableCollection<PHONG> DanhSachPhong
+        {
+            get => _danhSachPhong;
+            set
+            {
+                _danhSachPhong = value;
+                OnPropertyChanged(nameof(DanhSachPhong));
+            }
+        }
+
+        // Command xử lý tìm kiếm
+        public ICommand TraCuuCommand { get; set; }
+
+        public TraCuuPhongViewModel()
+        {
+            DanhSachPhong = new ObservableCollection<PHONG>();
+            TraCuuCommand = new RelayCommand<object>(CanExecuteTraCuu, TraCuuPhong);
+        }
+
+        private bool CanExecuteTraCuu(object parameter)
+        {
+            return true; // Luôn cho phép tìm kiếm
+        }
+
+        private void TraCuuPhong(object parameter)
+        {
+            var danhSachPhongTimThay = DataProvider.Instance.DB.PHONGs.Where(p =>
+                (string.IsNullOrEmpty(MaPhong) || p.MaPhong.Contains(MaPhong)) &&
+                (string.IsNullOrEmpty(TenPhong) || p.TenPhong.Contains(TenPhong)) &&
+                (string.IsNullOrEmpty(MaLoaiPhong) || p.MaLoaiPhong.Contains(MaLoaiPhong)) &&
+                (string.IsNullOrEmpty(TenLoaiPhong) || p.LOAIPHONG.TenLoaiPhong.Contains(TenLoaiPhong)) &&
+                (string.IsNullOrEmpty(GhiChu) || p.GhiChu.Contains(GhiChu))
+            ).ToList();
+
+            DanhSachPhong.Clear();
+            foreach (var phong in danhSachPhongTimThay)
+            {
+                DanhSachPhong.Add(phong);
+            }
+        }
+    }
+}
