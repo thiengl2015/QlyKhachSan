2025ce93adb233bfb43449ef16c36c1beaadc5c1
Phan Võ Mỹ Huyền <22520591@gm.uit.edu.vn>
2025-06-02 22:32:52 +0700

    UI Thay đổi số lượng khách tối đa

---
+++ QlyKhachSan/View/ThayDoiSoLuongKhachWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace QlyKhachSan.View
+{
+    public partial class ThayDoiSoLuongKhachWindow : Window
+    {
+        public ThayDoiSoLuongKhachWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel.ThayDoiSoLuongKhachViewModel();
+        }
+
+        private void Thoat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ QlyKhachSan/ViewModel/ThayDoiSoLuongKhachViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace QlyKhachSan.ViewModel
+{
+    public class ThayDoiSoLuongKhachViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> LoaiPhongList { get; set; }
+
+        private string _loaiPhongSelected;
+        public string LoaiPhongSelected
+        {
+            get => _loaiPhongSelected;
+            set
+            {
+                _loaiPhongSelected = value;
+                OnPropertyChanged(nameof(LoaiPhongSelected));
+                // Cập nhật số lượng hiện tại khi chọn loại phòng
+                SoLuongHienTai = "4"; // sau này load từ DB
+            }
+        }
+
+        private string _soLuongHienTai;
+        public string SoLuongHienTai
+        {
+            get => _soLuongHienTai;
+            set
+            {
+                _soLuongHienTai = value;
+                OnPropertyChanged(nameof(SoLuongHienTai));
+            }
+        }
+
+        private string _soLuongMoi;
+        public string SoLuongMoi
+        {
+            get => _soLuongMoi;
+            set
+            {
+                _soLuongMoi = value;
+                OnPropertyChanged(nameof(SoLuongMoi));
+            }
+        }
+
+        public ICommand LuuCommand { get; }
+
+        public ThayDoiSoLuongKhachViewModel()
+        {
+            LoaiPhongList = new ObservableCollection<string> { "Phòng đơn", "Phòng đôi", "Phòng VIP" };
+            LuuCommand = new RelayCommand(Luu);
+        }
+
+        private void Luu()
+        {
+            System.Windows.MessageBox.Show($"Đã cập nhật số lượng khách tối đa cho '{LoaiPhongSelected}' thành: {SoLuongMoi}");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
