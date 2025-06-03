49244b93ce6d2cbc34f29fba1c62fdddfff1d10a
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-06-03 22:17:20 +0700

    Thay đổi đơn giá phòng

--- QlyKhachSan/ViewModel/ThayDoiDonGiaViewModel.cs
+++ QlyKhachSan/ViewModel/ThayDoiDonGiaViewModel.cs
@@ -1,39 +1,84 @@
-﻿using System.ComponentModel;
+﻿using QlyKhachSan.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace QlyKhachSan.ViewModel
 {
-    public class ThayDoiDonGiaViewModel : INotifyPropertyChanged
+    public class ThayDoiDonGiaViewModel : BaseViewModel
     {
-        private string _donGia;
-        public string DonGia
+        private int _donGiaHienTai;
+        public int DonGiaHienTai
         {
-            get => _donGia;
+            get => _donGiaHienTai;
             set
             {
-                _donGia = value;
-                OnPropertyChanged(nameof(DonGia));
+                _donGiaHienTai = value;
+                OnPropertyChanged(nameof(DonGiaHienTai));
             }
         }
 
-        public ICommand ThayDoiCommand { get; }
+        private int donGiaMoi;
+        public int DonGiaMoi
+        {
+            get => donGiaMoi;
+            set
+            {
+                donGiaMoi = value;
+                OnPropertyChanged(nameof(DonGiaMoi));
+            }
+        }
+
+        private ObservableCollection<LOAIPHONG> loaiPhongList;
+        public ObservableCollection<LOAIPHONG> LoaiPhongList
+        {
+            get => loaiPhongList;
+            set
+            {
+                loaiPhongList = value;
+                OnPropertyChanged(nameof(LoaiPhongList));
+            }
+        }
+
+        private LOAIPHONG loaiPhongSelected;
+        public LOAIPHONG LoaiPhongSelected
+        {
+            get => loaiPhongSelected;
+            set
+            {
+                loaiPhongSelected = value;
+                OnPropertyChanged(nameof(LoaiPhongSelected));
+                if (loaiPhongSelected != null)
+                {
+                    DonGiaHienTai = loaiPhongSelected.DonGia ?? 0;
+                }
+            }
+        }
+
+        public ICommand LuuCommand { get; set; }
 
         public ThayDoiDonGiaViewModel()
         {
-            ThayDoiCommand = new RelayCommand(ThayDoi);
-            DonGia = "<Giá trị cũ>"; // Giá trị mặc định
+            LoaiPhongList = new ObservableCollection<LOAIPHONG>(DataProvider.Instance.DB.LOAIPHONGs);
+            LoaiPhongSelected = LoaiPhongList.FirstOrDefault();
+
+            LuuCommand = new RelayCommand<object>((p) => CanExecuteLuu(), (p) => LuuDonGia());
         }
 
-        private void ThayDoi()
+        private bool CanExecuteLuu()
         {
-            // Thực hiện xử lý thay đổi đơn giá
-            System.Windows.MessageBox.Show($"Đã thay đổi đơn giá thành: {DonGia}");
+            return LoaiPhongSelected != null && DonGiaMoi > 0;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
+        private void LuuDonGia()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (LoaiPhongSelected != null)
+            {
+                LoaiPhongSelected.DonGia = DonGiaMoi;
+                DataProvider.Instance.DB.SaveChanges();
+                DonGiaHienTai = DonGiaMoi;
+            }
         }
     }
 }
