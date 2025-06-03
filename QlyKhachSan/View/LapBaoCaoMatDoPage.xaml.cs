7c0f59d539d5af52c1e714b0748fc52d2feffa53
DuyDanga <146685689+DuyDanga@users.noreply.github.com>
2025-06-03 16:14:22 +0700

    feature/Bao-cao-mat-do

---
+++ QlyKhachSan/View/LapBaoCaoMatDoPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using QlyKhachSan.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QlyKhachSan.View
+{
+    /// <summary>
+    /// Interaction logic for LapBaoCaoMatDoPage.xaml
+    /// </summary>
+    public partial class LapBaoCaoMatDoPage : Page
+    {
+        public LapBaoCaoMatDoPage()
+        {
+            InitializeComponent();
+            this.DataContext = new LapBaoCaoMatDoViewModel();
+            MessageBox.Show("LapBaoCaoMatDoPage Loaded");
+        }
+    }
+}
